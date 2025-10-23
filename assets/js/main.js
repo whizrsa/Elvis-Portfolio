b335c531af430e9e1ec62af683bf7c3bc5e8aa9a
Elvis Chimuse <elvischimuse@gmail.com>
2025-10-23 19:02:20 +0200

    SEO Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,24 +167,7 @@
 
   });
 
-  /**
-   * Init swiper sliders
-   */
-  function initSwiper() {
-    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
-      let config = JSON.parse(
-        swiperElement.querySelector(".swiper-config").innerHTML.trim()
-      );
-
-      if (swiperElement.classList.contains("swiper-tab")) {
-        initSwiperWithCustomPagination(swiperElement, config);
-      } else {
-        new Swiper(swiperElement, config);
-      }
-    });
-  }
-
-  window.addEventListener("load", initSwiper);
+  // Swiper removed - not used on main page
 
   /**
    * Correct scrolling position upon page load for URLs containing hash links.
